5ee56db56e8456346b8146f37c255953be0fea34
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2024-01-26 14:28:38 -0300

    melhorando a consulta

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
         ConsultaCep consultaCep = new ConsultaCep();
-        Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
-        System.out.println(novoEndereco);
+
+        // CEP para teste: "01001000"
+        System.out.println("Digite um número de CEP para consulta: ");
+        var cep = leitura.nextLine();
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
     }
 }
